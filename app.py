6afab4ebd4f93b375fc806a87b564f21b7beb302
Farhaz <farhaz@gmail.com>
2026-01-30 11:20:47 -0500

    Changed App code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deployed via CI/CD Pipeline with Docker!"
+    return "Deployed via CI/CD Pipeline with Docker! by Abu Farhaz"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
